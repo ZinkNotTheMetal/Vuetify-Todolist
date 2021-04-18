6d386d8a4be3efe2167c046b05c018ec632f9851
William Zink <williamdzink@gmail.com>
2021-04-17 20:27:07 -0500

    Add todo list items foreach over local items

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,13 +1,55 @@
 <template>
 
     <div class="pa-6">
-        <span>On TODO Page</span>
+        
+        <v-list
+            subheader
+            two-line
+            flat
+        >
+
+            <v-list-item-group
+                v-model="settings"
+                multiple
+            >
+                
+                <v-list-item
+                    v-for="item in todo_items"
+                    :key="item.id"
+                >
+                <template>
+                    <v-list-item-action>
+                    <v-checkbox
+                        :input-value="item.isDone"
+                        color="primary"
+                    ></v-checkbox>
+                    </v-list-item-action>
+
+                    <v-list-item-content>
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </template>
+                </v-list-item>
+
+            </v-list-item-group>
+        </v-list>
+
     </div>
     
 </template>
 
 <script>
 export default {
-    name: 'Todo'
+    name: 'Todo',
+    data() {
+        return {
+            todo_items: [
+                { id: 1, title: 'wake up', isDone: false },
+                { id: 2, title: 'peel bananas', isDone: false },
+                { id: 3, title: 'eat bananas', isDone: false },
+                { id: 4, title: 'poo bananas', isDone: false },
+            ]
+        }
+    },
 }
 </script>
